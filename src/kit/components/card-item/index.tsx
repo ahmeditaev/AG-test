2b882e0e8058c0dbc5466d00d50c6385f1185913
Akhmed Itaev <ahmeditaev@gmail.com>
2021-01-22 02:32:53 +0600

    updated card-item component

--- src/kit/components/card-item/index.tsx
+++ src/kit/components/card-item/index.tsx
@@ -5,27 +5,37 @@ import './index.scss'
 
 
 interface CardItemProps {
-
+  city: string
+  title: string
+  category: string
+  price: number
 }
 
 const CardItem: React.FC<CardItemProps> = (props: CardItemProps) => {
-  // const {} = props
+  const {
+    city,
+    title,
+    category,
+    price
+  } = props
   return (
     <div className="card-item">
       <div className="image-wrap">
-        <span className="overlay"></span>
+        <span className="overlay" />
         <img width="270" height="180" src={PreviewImage} alt="Preview-image" className="card-item__image" />
       </div>
-      <h3 className="card-item__city">London</h3>
+      <h3 className="card-item__city">
+        {city}
+      </h3>
       <h2 className="card-item__title">
-        Affiliate Marketing - A Beginner's Guide to Earning Online
+        {title}
       </h2>
       <div className="info">
         <span className="card-item__category">
-          Architecture
+          {category}
         </span>
         <span className="card-item__price">
-          $115
+          ${price}
         </span>
       </div>
     </div>
