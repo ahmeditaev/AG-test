039a106db89054f9a182c08f4a106459ef15a3e2
Akhmed Itaev <ahmeditaev@gmail.com>
2021-01-24 06:12:20 +0600

    updated app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,71 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import CardItem from "./../kit/components/card-item";
-import CustomSelect from "./../kit/components/select";
-import Checkbox from "./../kit/components/checkbox";
+import CustomSelect, {OptionValues} from "./../kit/components/select";
 import MultiRangeSlider from "./../kit/components/multi-range-slider";
-import './App.scss';
 import SidebarSection from '../kit/components/sidebar-section';
 import Button from '../kit/components/button';
+import {OptionsType, ValueType} from "react-select";
+import {useDispatch, useSelector} from "react-redux";
+import {productsOperations} from '../redux/Products'
+import {citiesOperations} from "../redux/Cities";
+import {IProduct} from "../redux/Products/models"
+import {categoriesOperations} from "../redux/Categories";
+import {transformValueToOptionValue} from "../utils/transformValueToOptionValue";
+import CategoryList from '../kit/components/category-list';
+
+import './App.scss';
+import {ICategory} from "../redux/Categories/models";
 
 interface Props {
 
 }
 
+
 const App: React.FC<Props> = (props: Props) => {
-  const [checkboxState, setState] = useState(false)
+  const {fetchProducts} = productsOperations
+  const {fetchCities, onChangeCurrentCity} = citiesOperations
+  const {fetchCategories} = categoriesOperations
+
+  const productsState: any = useSelector<any>(state => state.Products)
+  const citiesState: any = useSelector<any>(state => state.Cities)
+  const categoriesState: any = useSelector<any>(state => state.Categories)
+
+  const dispatch = useDispatch()
+
+  const {products, loading: productsLoading} = productsState
+  const {cities, loading: citiesLoading, currentCity} = citiesState
+  const {categories, loading: categoriesLoading} = categoriesState
+
+  useEffect(() => {
+    !products && dispatch(fetchProducts())
+    !cities && dispatch(fetchCities())
+    !categories && dispatch(fetchCategories())
+  }, [])
+
+  if (productsLoading || citiesLoading || categoriesLoading) {
+    return <div>loading...</div>
+  }
+
+  if (!products || products.length === 0) {
+    return <div>No data</div>
+  }
+
+  const options: OptionsType<OptionValues> = cities.map(transformValueToOptionValue)
+
   return (
     <div className="App">
       <div className="container">
         <div className="content">
           <div className="sidebar">
             <SidebarSection title="City">
-              <CustomSelect/>
+              <CustomSelect
+                value={currentCity ? transformValueToOptionValue(currentCity) : null}
+                options={options}
+                onChange={(option: ValueType<OptionValues, boolean>) => dispatch(onChangeCurrentCity(option))}
+              />
             </SidebarSection>
             <SidebarSection title="Categories">
-              <ul className="category-list">
-                <li className="category-list__item">
-                  <Checkbox
-                    id="test"
-                    isChecked={checkboxState}
-                    label="Business"
-                    numberValue="11"
-                    onClick={() => setState(!checkboxState)}
-                  />
-                </li>
-              </ul>
+              <CategoryList />
             </SidebarSection>
             <SidebarSection title="Price">
               <div className="price-wrap">
@@ -47,12 +80,18 @@ const App: React.FC<Props> = (props: Props) => {
           </div>
           <div className="main">
             <div className="card-list">
-              <CardItem/>
-              <CardItem/>
-              <CardItem/>
-              <CardItem/>
-              <CardItem/>
-              <CardItem/>
+              {products.map((item: IProduct, idx: number) => {
+                const findCategoryById = categories.find((category: ICategory) => item.category === category.id)
+                return (
+                  <CardItem
+                    key={idx}
+                    city="London"
+                    title={item.name}
+                    category={findCategoryById ? findCategoryById.name : ''}
+                    price={item.price}
+                  />
+                )
+              })}
             </div>
           </div>
         </div>
